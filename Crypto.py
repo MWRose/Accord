4b9541c65c3d484e627a0a0b61d93de5b76ef417
MWRose <mwra2018@mymail.pomona.edu>
2020-10-07 11:12:39 -0700

    initial commit

---
+++ Crypto.py
@@ -0,0 +1,27 @@
+def aes_encrypt(msg: bytes, aes_key: bytes) -> bytes:
+    """
+    Takes msg and encrypts it under aes_key
+    """
+    
+    return
+
+
+def aes_decrypt(msg: bytes, aes_key: bytes) -> bytes:
+    """
+    Takes msg and decrypts it with aes_key
+    """
+    return
+
+
+def rsa_encrypt(msg: bytes, public_key: bytes) -> bytes:
+    """
+    Takes msg and encrypts it under public_key
+    """
+    return
+
+
+def rsa_decrypt(msg: bytes, public_key: bytes) -> bytes:
+    """
+    Takes msg and decrypts it under private_key
+    """
+    return
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# Accord
+Handshake btw client and server
+- Initial handhsake 
+- Every time a new private message connection is created 
+- Proper forwarding A->S, S->B, B->S, B->A
+
+Database
+
+
+https://trello.com/invite/b/GLhojP1T/4e4fb2dbbe15513420e0ab415ca7347f/accord
+
+Signature package for digital signatures using Pycryptodome: https://pycryptodome.readthedocs.io/en/latest/src/signature/signature.html
