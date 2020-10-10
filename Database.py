c1d5572554ff8f3e42c890c316c067839b551397
Pavle Rohalj <pavlerohalj@users.noreply.github.com>
2020-10-10 01:28:27 -0700

    Update database code

--- Database.py
+++ Database.py
@@ -1,12 +1,13 @@
 import sqlite3
 from sqlite3 import Error
 
+DATABASE_FILE = r"Accord.db"
 
-def initalize_database(db_file):
+def initalize_database():
     """ create a database connection to a SQLite database """
     conn = None
     try:
-        conn = sqlite3.connect(db_file)
+        conn = sqlite3.connect(DATABASE_FILE)
         cursor = conn.cursor()
         cursor.execute("""CREATE TABLE users
                         (email text)    
@@ -17,37 +18,39 @@ def initalize_database(db_file):
         if conn:
             conn.close()
 
-def check_email(db_file,user:str) -> bool:
+def check_email(email: str) -> bool:
     """ checking if email exists in the SQLite database """
     try:
-        conn = sqlite3.connect(r"Accord.db")
+        conn = sqlite3.connect(DATABASE_FILE)
         cursor = conn.cursor()
-        for query in c.fetchall():
-            if user in query:
-                return True
-            else:
-                return False
-    except Error as e:
+        cursor.execute("SELECT * FROM users WHERE email=?", (email,))
+        result = cursor.fetchone()
+        return result is not None
+    except Error:
         return False
+    print("here")
 
 
-def add_user_email(db_file,user:str) -> bool:
+def add_user_email(email:str) -> bool:
     """ adding user email into the SQLite database """
     try:
-        conn = sqlite3.connect(r"Accord.db")
+        conn = sqlite3.connect(DATABASE_FILE)
         cursor = conn.cursor()
         sqlite_insert_with_param = """
                     INSERT INTO users
                     (email)
                     VALUES(?);
                     """
-        cursor.execute(sqlite_insert_with_param,user)
+        cursor.execute(sqlite_insert_with_param, (email,))
         conn.commit()
         return True
-    except Error as e:
+    except Error:
         return False
     
 
 if __name__ == '__main__':
-    initalize_database(r"Accord.db")
+    # initalize_database(r"Accord.db")
+    print(check_email("pavle.rohalj@pomona.edu"))
+    print(add_user_email("pavle.rohalj@pomona.edu"))
+    print(check_email("pavle.rohalj@pomona.edu"))
     
