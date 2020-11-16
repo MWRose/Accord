e0d9c05bf7af705514e2e3e2d0f2fe468dbd755c
Pavle Rohalj <pavlerohalj@users.noreply.github.com>
2020-11-15 20:12:00 -0800

    Update private messaging tests

--- src/TestPrivateMessaging.py
+++ src/TestPrivateMessaging.py
@@ -17,9 +17,9 @@ class TestPrivateMessaging(TestCase):
         contacts = {'bob': {'public_key': b'-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAoNd3g3mkIOmsKHfzAEDs\n34oqVz71+ZhbyyQ4wfkKjWHCbz9js7iXt5uerTLso+CivkxRE4cx3stUWFtIc9Jq\nlAiOrkPjuOZrW1qvTUI/g4EuNpBIN1UKrQTjnInVLLXD8ko5fgu38xlPMhOU2Owi\nSo6EXskByHFQVuktQxmVIdGvLf8r5sBAImtdsROxR/owjiZ4mGz7yLiOKwV9Hik4\ndQMpkHduCH5H6P0kki1B2+3pnIP3vdAXoSvfxUcL9xkb8+KpX/idwrB06hftBO9i\n6MmknI5RFwquirF1bxnuEMByWTsywvg50qCyaM+TsgMtucUkwCnkDvSL7vk5IJe7\nQwIDAQAB\n-----END PUBLIC KEY-----', 'aes_key': b'/\x9ei\xa0\xa9\xeclp;r[\xea\xad\x99\x009', 'hmac_key': b'\x1ca\x93\xe5\x94?\xe4C\xa2N\xa4\xe1\xe5:\xceB'}}
 
         # Expected
-        enc_msg = b"\x06\xef\xd2\xf5\xa6\x93\xed']\x81\xc4\xfb\x84K\xbe\xe0"
-        iv = b'\xa4\xd58\xee\x8fu\x937y\xb6\xfd\x13\xf8\xe8j@'
-        tag = "22c28febe02f33793d80696e88b50ba188c3f0aafc76f4e1ea0b094be80b50d2"
+        enc_msg = b'q>\x1f\x9b\xab1\xf7]\x8a\x12\x0c\xd5\x91&\xc6\xef'
+        iv = b'\xcb\xca\xa0U[\xf4\x15\xa8\x1fY\xf3\xc98v\xd0\xf0'
+        tag = '6745d330f6b9ca03c972f617d322b733aa67ecd8a81ab5cc310c658b017dd9fd'
         enc_msg_b64 = base64.b64encode(enc_msg)
         iv_b64 = base64.b64encode(iv)
 
@@ -28,7 +28,7 @@ class TestPrivateMessaging(TestCase):
         
         # Actual test
         Receive.receive_direct(parsed_request.data, contacts)
-        mock_print.assert_called_with(sender + " to " + recipient + ": " + message)
+        mock_print.assert_called_with(sender + ": " + message)
 
     def test_receive_bad_tag(self):
         # Input
