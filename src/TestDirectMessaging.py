8e8141e01be05e976ee6e27a3f7f08c951a59833
adelineyu <adeline.chinyuan.yu@gmail.com>
2020-12-04 08:37:55 -0800

    Timestamps in TestDirectMessaging.py

--- src/TestDirectMessaging.py
+++ src/TestDirectMessaging.py
@@ -4,6 +4,7 @@ import Receive
 import Requests
 import Send
 import base64
+import datetime
 
 
 class TestPrivateMessaging(TestCase):
@@ -22,15 +23,16 @@ class TestPrivateMessaging(TestCase):
         # Expected
         enc_msg = b'q>\x1f\x9b\xab1\xf7]\x8a\x12\x0c\xd5\x91&\xc6\xef'
         iv = b'\xcb\xca\xa0U[\xf4\x15\xa8\x1fY\xf3\xc98v\xd0\xf0'
+        timestamp = str(datetime.datetime.now().timestamp())
         tag = '6745d330f6b9ca03c972f617d322b733aa67ecd8a81ab5cc310c658b017dd9fd'
         enc_msg_b64 = base64.b64encode(enc_msg)
         iv_b64 = base64.b64encode(iv)
 
-        request = Requests.direct_message(sender, recipient, str(enc_msg_b64), str(iv_b64), tag)
+        request = Requests.direct_message(sender, recipient, str(enc_msg_b64), str(iv_b64), timestamp, tag)
         parsed_request = Requests.parse_request(request)
 
         # Actual test
-        Receive.receive_direct(parsed_request.data, contacts)
+        Receive.receive_direct(parsed_request.data, contacts, timestamp)
         mock_print.assert_called_with(sender + ": " + message)
 
     def test_receive_bad_tag(self):
@@ -47,17 +49,18 @@ class TestPrivateMessaging(TestCase):
         # Expected
         enc_msg = b"\x06\xef\xd2\xf5\xa6\x93\xed']\x81\xc4\xfb\x84K\xbe\xe0"
         iv = b'\xa4\xd58\xee\x8fu\x937y\xb6\xfd\x13\xf8\xe8j@'
+        timestamp = str(datetime.datetime.now().timestamp())
         tag = ""
         enc_msg_b64 = base64.b64encode(enc_msg)
         iv_b64 = base64.b64encode(iv)
 
-        request = Requests.direct_message(sender, recipient, str(enc_msg_b64), str(iv_b64), tag)
+        request = Requests.direct_message(sender, recipient, str(enc_msg_b64), str(iv_b64), timestamp, tag)
         parsed_request = Requests.parse_request(request)
         parsed_request = Requests.parse_request(request)
 
         # Actual test
         with self.assertRaises(Exception) as context:
-            Receive.receive_direct(parsed_request.data, contacts)
+            Receive.receive_direct(parsed_request.data, contacts, timestamp)
 
     @patch('Crypto_Functions.aes_encrypt')
     @patch('Crypto_Functions.hmac')
@@ -83,13 +86,13 @@ class TestPrivateMessaging(TestCase):
         enc_msg = b"\xfd\xbdL\xb0\xb6!x%\xd2\x01]\xf2\x9a^vM"
         iv = b"\x8a\x8eP\x9aU7;\xe8\xb8,M\xb7\xa5\x97\x14\xc3"
         mock_aes_encrypt.return_value = (enc_msg, iv)
+        timestamp = str(datetime.datetime.now().timestamp())
         tag = "c5c8e2d7baad57b7819bc6748bcd14cf91dc9ad5e0731fa8c26f95959ebbce01"
         mock_hmac.return_value = tag
-
         # Expected
-        enc_msg_b64 = base64.b64encode(enc_msg)
+        enc_msg_b64 = base64.b64encode(enc_msg) 
         iv_b64 = base64.b64encode(iv)
-        request = Requests.direct_message(sender, recipient, str(enc_msg_b64), str(iv_b64), tag)
+        request = Requests.direct_message(sender, recipient, str(enc_msg_b64), str(iv_b64), timestamp, tag)
 
         # Actual test
         Send.send_direct(sender, recipient, contacts, str(enc_msg_b64), mock_socket)
